411cf1b91320f34291b16a2d846ad2b39ae4f7b2
avikj <avikjain12345@gmail.com>
2016-03-13 13:33:25 -0700

    Added molar mass calculations and stored secret data in separate file

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 var login = require("facebook-chat-api");
-var wolfram = require('wolfram').createClient("WOLFRAM_API_KEY");
+var wolfram = require('wolfram');
 var pt = require("periodic-table");
 var fs = require("fs");
+var secretData = JSON.parse(fs.readFileSync("secretData.json"));
+console.log(secretData);
+var wolframClient = wolfram.createClient(secretData.WOLFRAM_API_KEY);
+
 var elementData = pt.all();
-login({email: "loquaciousmeow@gmail.com", password: "FACEBOOK_ACCOUNT_PASSWORD"}, function callback (err, api) {
+
+login({email: secretData.FACEBOOK_EMAIL, password: secretData.FACEBOOK_PASSWORD}, function callback (err, api) {
     if(err) return console.error(err);
  
     api.listen(function callback(err, message) {
+    	// help message
+    	if(message.body.trim() == "help"){
+    		api.sendMessage("Available commands:\n\t<element symbol> - finds the element with that symbol and returns relevant information"
+    			+"\n\tmm <compound> - returns the molar mass of a compound. Requires proper capitalization."
+    			+"\n\tbalance <equation> - returns a balanced form of a chemical equation. Requires proper capitalization."
+    			+"\n\thelp - displays this message.", message.threadID);
+    		return;
+    	}
+
+    	// balance chemical equations
     	if(message.body.startsWith("balance")){
-			wolfram.query(message.body, function(err, result) {
+			wolframClient.query(message.body, function(err, result) {
+				if(err) throw err;
 				try{
 					api.sendMessage(result[1].subpods[0].value, message.threadID);
 				}catch(e){
@@ -17,7 +33,21 @@ login({email: "loquaciousmeow@gmail.com", password: "FACEBOOK_ACCOUNT_PASSWORD"}
 			});
 			return;
     	}
-    	var done = false;
+
+    	// find molar mass
+    	if(message.body.startsWith("mm")){
+    		wolframClient.query("molar mass "+message.body.substring(2), function(err, result) {
+				if(err) throw err;
+				try{
+					api.sendMessage("The molar mass of "+message.body.substring(2)+" is "+result[1].subpods[0].value+".", message.threadID);
+				}catch(e){
+					api.sendMessage("There was an error computing the molar mass. Make sure that you use proper capitalization.", message.threadID)
+				}
+			});
+			return;
+    	}
+
+    	// find element by symbol
     	for(var i = 0; i < elementData.length; i++){
 			if(elementData[i].symbol.toUpperCase()==message.body.toUpperCase()){
 				api.sendMessage({
